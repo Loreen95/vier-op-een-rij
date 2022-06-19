d6a35dc85e0ce588aae2bd1afaae02852376ae99
Loreen95 <lisahakhoff@ziggo.nl>
2022-06-19 22:02:34 +0200

    Vier op een rij
    Vier op een rij

---
+++ Vier Op Een Rij/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿
+namespace Vier_Op_Een_Rij
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pBord = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.coinFall = new System.Windows.Forms.Timer(this.components);
+            this.label12 = new System.Windows.Forms.Label();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.spelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nieuwSpelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.spelregelsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.overToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pBord)).BeginInit();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pBord
+            // 
+            this.pBord.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pBord.Location = new System.Drawing.Point(27, 27);
+            this.pBord.Name = "pBord";
+            this.pBord.Size = new System.Drawing.Size(762, 654);
+            this.pBord.TabIndex = 0;
+            this.pBord.TabStop = false;
+            this.pBord.Paint += new System.Windows.Forms.PaintEventHandler(this.pBordDraw);
+            this.pBord.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pBord_MouseDown);
+            this.pBord.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pBord_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(924, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(924, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(924, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(924, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(813, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "X";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(813, 64);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 20);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Y";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(813, 101);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 20);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "X/100";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(813, 137);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(71, 20);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "X/100 a";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(813, 171);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(56, 20);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Y/100";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(924, 137);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(67, 20);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "label10";
+            // 
+            // coinFall
+            // 
+            this.coinFall.Interval = 50;
+            this.coinFall.Tick += new System.EventHandler(this.coinFall_Tick);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(813, 236);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(0, 20);
+            this.label12.TabIndex = 13;
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.spelToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(1002, 24);
+            this.menuStrip2.TabIndex = 14;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // spelToolStripMenuItem
+            // 
+            this.spelToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nieuwSpelToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.spelToolStripMenuItem.Name = "spelToolStripMenuItem";
+            this.spelToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.spelToolStripMenuItem.Text = "Spel";
+            // 
+            // nieuwSpelToolStripMenuItem
+            // 
+            this.nieuwSpelToolStripMenuItem.Name = "nieuwSpelToolStripMenuItem";
+            this.nieuwSpelToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.nieuwSpelToolStripMenuItem.Text = "Nieuw spel";
+            this.nieuwSpelToolStripMenuItem.Click += new System.EventHandler(this.nieuwSpelToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.spelregelsToolStripMenuItem,
+            this.overToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // spelregelsToolStripMenuItem
+            // 
+            this.spelregelsToolStripMenuItem.Name = "spelregelsToolStripMenuItem";
+            this.spelregelsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.spelregelsToolStripMenuItem.Text = "Spelregels";
+            this.spelregelsToolStripMenuItem.Click += new System.EventHandler(this.spelregelsToolStripMenuItem_Click);
+            // 
+            // overToolStripMenuItem
+            // 
+            this.overToolStripMenuItem.Name = "overToolStripMenuItem";
+            this.overToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.overToolStripMenuItem.Text = "Over";
+            this.overToolStripMenuItem.Click += new System.EventHandler(this.overToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1002, 729);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pBord);
+            this.Controls.Add(this.menuStrip2);
+            this.Name = "Form1";
+            this.Text = "4 Op Een Rij";
+            ((System.ComponentModel.ISupportInitialize)(this.pBord)).EndInit();
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pBord;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Timer coinFall;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem spelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nieuwSpelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem spelregelsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem overToolStripMenuItem;
+    }
+}
+
---
+++ Vier Op Een Rij/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vier_Op_Een_Rij
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            startBord();
+        }
+
+        Brush[,] arrayKleuren = new Brush[6, 7];
+        Pen tekenpotlood = new Pen(Color.Black, 5);
+        bool boolBeurt = true;
+        bool muntVallen;
+        int intKolom, intRij;
+        int intHuidigeRij;
+
+        private void startBord()
+        {
+            for (int rij = 0; rij < arrayKleuren.GetLength(0); rij++)
+            {
+                for (int kolom = 0; kolom < arrayKleuren.GetLength(1); kolom++)
+                {
+                    arrayKleuren[rij, kolom] = Brushes.White;
+                }
+            }
+        }
+
+        private void pBordDraw(object sender, PaintEventArgs e)
+        {
+            Graphics bord = e.Graphics;
+            for (int rij = 0; rij < arrayKleuren.GetLength(0); rij++)
+            {
+                for (int kolom = 0; kolom < arrayKleuren.GetLength(1); kolom++)
+                {
+                    bord.DrawEllipse(tekenpotlood, 10 + kolom * 107, 10 + rij * 107, 100, 100);
+                    bord.FillEllipse(arrayKleuren[rij, kolom], 10 + kolom * 107, 10 + rij * 107, 100, 100);
+                }
+            }
+        }
+        // ZONDER VAL-TIMER
+        /* private void beurtBijhouden()
+         {
+             if (boolBeurt == true && arrayKleuren[intRij, intKolom] == Brushes.White)
+             {
+                 if (intRij == 5)
+                 {
+                     arrayKleuren[intRij, intKolom] = Brushes.Red; 
+                     boolBeurt = false;
+
+                 }
+                 else if (arrayKleuren[intRij + 1, intKolom] != Brushes.White)
+                 {
+                     arrayKleuren[intRij, intKolom] = Brushes.Red; 
+                     boolBeurt = false;
+                 }
+             }
+             else if (boolBeurt == false && arrayKleuren[intRij, intKolom] == Brushes.White)
+             {
+                 if (intRij == 5)
+                 {
+                     arrayKleuren[intRij, intKolom] = Brushes.Yellow; 
+                     boolBeurt = true;
+                 }
+                 else if (arrayKleuren[intRij + 1, intKolom] != Brushes.White)
+                 {
+                     arrayKleuren[intRij, intKolom] = Brushes.Yellow; 
+                     boolBeurt = true;
+                 }
+             } 
+             pBord.Invalidate();
+         } */
+
+        private void pBord_MouseMove(object sender, MouseEventArgs e)
+        {
+            label1.Text = e.X.ToString();
+            label2.Text = e.Y.ToString();
+            label3.Text = (e.X / 100).ToString();
+            label4.Text = (e.Y / 100).ToString();
+        }
+
+        int huidigeRij = 0;
+        int intHuidigeKolom = 0;
+        bool beurtTimer;
+
+        private void coinFall_Tick(object sender, EventArgs e)
+        {
+            #region timerTick
+            muntVallen = true;
+            if (!beurtTimer)
+            {
+                arrayKleuren[intHuidigeRij, intHuidigeKolom] = Brushes.Red;
+            }
+
+            else
+            {
+                arrayKleuren[intHuidigeRij, intHuidigeKolom] = Brushes.Yellow;
+            }
+            if (intHuidigeRij > 0)
+            {
+                arrayKleuren[intHuidigeRij - 1, intHuidigeKolom] = Brushes.White;
+            }
+
+            if (intHuidigeRij == 5)
+            {
+                coinFall.Stop();
+                winCheck();
+                muntVallen = false;
+                beurtTimer = !beurtTimer;
+            }
+
+            else if (arrayKleuren[intHuidigeRij + 1, intHuidigeKolom] != Brushes.White)
+            {
+                coinFall.Stop();
+                winCheck();
+                muntVallen = false;
+                beurtTimer = !beurtTimer;
+            }
+
+            intHuidigeRij++;
+            pBord.Invalidate();
+            #endregion
+        }
+
+        private void checkRij(Brush kleur)
+        {
+            int intTellerRij = 0;
+            for (int j = 0; j < 6; j++)
+            {
+                for (int i = 0; i < 7; i++)
+                {
+                    if (arrayKleuren[j, i] == kleur)
+                    {
+                        intTellerRij++;
+                        if (intTellerRij == 4)
+                        {
+                            if (boolBeurt == false)
+                            {
+                                MessageBox.Show("De winnaar is Geel!");
+                                Application.Restart();
+                            }
+                            else
+                            {
+                                MessageBox.Show("De winnaar is Rood!");
+                                Application.Restart();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        intTellerRij = 0;
+                    }
+                }
+            }
+        }
+
+        private void checkKolom(Brush kleur)
+        {
+            int intTellerKolom = 0;
+            for (int j = 0; j < 7; j++)
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    if (arrayKleuren[i, j] == kleur)
+                    {
+                        intTellerKolom++;
+                        if (intTellerKolom == 4)
+                        {
+                            if (boolBeurt == false)
+                            {
+                                MessageBox.Show("De winnaar is Geel!");
+                                Application.Restart();
+                            }
+                            else
+                            {
+                                MessageBox.Show("De winnaar is Rood!");
+                                Application.Restart();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        intTellerKolom = 0;
+                    }
+                }
+            }
+        }
+
+        private void winCheck()
+        {
+            boolBeurt = !boolBeurt;
+            pBord.Invalidate();
+
+            // Horizontaal
+            checkRij(Brushes.Red);
+            checkRij(Brushes.Yellow);
+
+            // Verticaal
+            checkKolom(Brushes.Red);
+            checkKolom(Brushes.Yellow);
+        }
+
+        private void pBord_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (!muntVallen)
+            {
+                intKolom = (e.X + 90 - (e.X / 100) * 5) / 100 - 1;
+                //intRij = (e.Y + 90 - (e.Y / 100) * 5) / 100 - 1;
+
+                coinFall.Start();
+                intHuidigeRij = 0;
+                intHuidigeKolom = intKolom;
+                pBord.Invalidate();
+            }
+            else
+            {
+                MessageBox.Show("Wacht even.");
+            }
+            winCheck();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void spelregelsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Wanneer een speler 4 van dezelfde kleur op een rij heeft horizontaal of verticaal");
+        }
+
+        private void overToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Geschreven door Lisa Hakhoff, 2022");
+        }
+
+        private void nieuwSpelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
